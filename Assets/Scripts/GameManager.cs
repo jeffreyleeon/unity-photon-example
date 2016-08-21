e72eb9a32e439438802ce23114d131b7e770b5f1
Lee Chun On <jeffreyleeon@gmail.com>
2016-08-21 12:44:37 +0800

    Able to demonstrate a 2-players game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+
+namespace Com.FYP.DES {
+	public class GameManager : Photon.PunBehaviour {
+
+		public override void OnLeftRoom () {
+			print ("===========OnLeftRoom callback");
+			SceneManager.LoadScene (0);
+		}
+
+		public void LeaveRoom () {
+			print ("===========GameManager LeaveRoom");
+			PhotonNetwork.LeaveRoom ();
+		}
+
+		void LoadArena () {
+			if (!PhotonNetwork.isMasterClient) {
+				print ("PhotonNetwork : Trying to Load a level but we are not the master Client");
+			}
+			print ("PhotonNetwork : Loading Level : " + PhotonNetwork.room.playerCount);
+			PhotonNetwork.LoadLevel ("Room for " + PhotonNetwork.room.playerCount);
+		}
+
+		public override void OnPhotonPlayerConnected( PhotonPlayer other  )
+		{
+			Debug.Log( "OnPhotonPlayerConnected() " + other.name ); // not seen if you're the player connecting
+			if ( PhotonNetwork.isMasterClient ) {
+				Debug.Log( "OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient ); // called before OnPhotonPlayerDisconnected
+				LoadArena();
+			}
+		}
+
+
+		public override void OnPhotonPlayerDisconnected( PhotonPlayer other  )
+		{
+			Debug.Log( "OnPhotonPlayerDisconnected() " + other.name ); // seen when other disconnects
+			if ( PhotonNetwork.isMasterClient ) {
+				Debug.Log( "OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient ); // called before OnPhotonPlayerDisconnected
+				LoadArena();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+namespace Com.FYP.DES {
+
+	public class Launcher : Photon.PunBehaviour {
+		// This client's version number. Users are separated from each other by gameversion (which allows you to make breaking change)
+		string _gameVersion = "1";
+		bool isConnecting;
+
+		[Tooltip("The Ui Panel to let the user enter name, connect and play")]
+		public GameObject controlPanel;
+		[Tooltip("The UI Label to inform the user that the connection is in progress")]
+		public GameObject progressLabel;
+
+		void Start () {
+			SetLoading (false);
+		}
+
+		void Awake () {
+			PhotonNetwork.logLevel = PhotonLogLevel.Full;
+			PhotonNetwork.autoJoinLobby = false;
+			PhotonNetwork.automaticallySyncScene = true;
+		}
+
+		private void SetLoading (bool loading) {
+			progressLabel.SetActive(loading);
+			controlPanel.SetActive(!loading);
+		}
+
+		public void Connect () {
+			SetLoading (true);
+			isConnecting = true;
+			if (PhotonNetwork.connected) {
+				print ("====jeffrey JoinRandomRoom");
+				// #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed()
+				PhotonNetwork.JoinRandomRoom ();
+			} else {
+				print ("====jeffrey ConnectUsingSettings");
+				PhotonNetwork.ConnectUsingSettings (_gameVersion);
+			}
+		}
+
+		public override void OnConnectedToMaster () {
+			print ("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+			// #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()  
+			if (isConnecting) {
+				PhotonNetwork.JoinRandomRoom();
+			}
+		}
+
+		public override void OnDisconnectedFromPhoton () {
+			print ("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+			SetLoading (false);
+		}
+
+		public override void OnPhotonRandomJoinFailed (object[] codeAndMsg) {
+			print ("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 2}, null);");
+
+			// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+			PhotonNetwork.CreateRoom (
+				"Jeffrey roomName",
+				new RoomOptions () {
+					maxPlayers = 2
+				},
+				null
+			);
+		}
+
+		public override void OnJoinedRoom () {
+			print ("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+			PrintRooms ();
+			progressLabel.SetActive(false);
+			controlPanel.SetActive(false);
+			if (PhotonNetwork.room.playerCount == 1) {
+				Debug.Log("We load the 'Room for 1' ");
+				// #Critical
+				// Load the Room Level. 
+				PhotonNetwork.LoadLevel("Room for 1");
+			}
+		}
+
+		private void PrintRooms () {
+			RoomInfo[] rooms = PhotonNetwork.GetRoomList ();
+			print ("Room count: " + rooms.Length);
+			for (int i = 0; i < rooms.Length; ++i) {
+				RoomInfo room = rooms [i];
+				print ("Room info: " + room.GetHashCode () + ":" + room.name);
+			}
+		}
+	}
+
+}
